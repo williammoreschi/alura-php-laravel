10a668ce54091303deb5278214ddf1092ae9ac1e
williammoreschi <williammoreschi@gmail.com>
2022-03-09 17:36:21 +0000

    mostrando os dados na view

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,14 +6,7 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller{
     public function index(Request $request){
-        $series = ["Breaking Bad","Elementary","Sherlock Holmes"];
-    
-        $html = "<ul>";
-        foreach ($series as $serie) {
-            $html .= "<li>{$serie}</li>";
-        }
-        $html .= "</ul>";
-    
-        return $html;
+        $series = ["Breaking Bad","Elementary","Sherlock Holmes"];    
+        return view('series.index',compact('series'));
     }
 }
