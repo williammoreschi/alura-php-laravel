b16dd750338b9a4c8ea7509ff93eac2d9c4f3b77
williammoreschi <williammoreschi@gmail.com>
2022-03-10 02:06:41 +0000

    desacoplando a validação para classe mais específica

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SeriesFormRequest;
 use App\Serie;
 use Illuminate\Http\Request;
 
@@ -19,11 +20,8 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(Request $request)
+    public function store(SeriesFormRequest $request)
     {
-        $request->validate([
-            'nome' => 'required|min:3'
-        ]);
 
         $serie = Serie::create($request->all());
         $request->session()->flash(
