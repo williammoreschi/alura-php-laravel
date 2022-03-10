c9d2f44fe7add51e596d4573f62fda43de8d919b
williammoreschi <williammoreschi@gmail.com>
2022-03-10 02:20:11 +0000

    tirando margin do layout

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -7,7 +7,7 @@ Adicionar Séries
 @section('conteudo')
 @if ($errors->any())
 <div class="alert alert-danger">
-    <ul>
+    <ul class="mb-0" >
         @foreach ($errors->all() as $error)
         <li>{{ $error }}</li>
         @endforeach
