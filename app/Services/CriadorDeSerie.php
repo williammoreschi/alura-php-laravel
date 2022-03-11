2436a80847b1d7634f0ae474680f872896c93639
williammoreschi <williammoreschi@gmail.com>
2022-03-11 18:42:34 +0000

    refatorando codigo de criar serie

--- app/Services/CriadorDeSerie.php
+++ app/Services/CriadorDeSerie.php
@@ -3,6 +3,8 @@
 namespace App\Services;
 
 use App\Serie;
+use App\Temporada;
+use Illuminate\Support\Facades\DB;
 
 class CriadorDeSerie
 {
@@ -11,16 +13,29 @@ class CriadorDeSerie
         int $qtd_temporadas,
         int $ep_por_temporada
     ): Serie {
+        DB::beginTransaction();
         $serie = Serie::create(["nome" => $nome]);
+        $this->criarTemporadas($serie, $qtd_temporadas, $ep_por_temporada);
+        DB::commit();
+        return $serie;
+    }
 
+    private function criarTemporadas(
+        Serie $serie,
+        int $qtd_temporadas,
+        int $ep_por_temporada
+    ): void {
         for ($i = 1; $i <= $qtd_temporadas; $i++) {
             $temporada = $serie->temporadas()->create(['numero' => $i]);
 
-            for ($j = 1; $j <= $ep_por_temporada; $j++) {
-                $temporada->episodios()->create(['numero' => $j]);
-            }
+            $this->criarEpisodios($temporada, $ep_por_temporada);
         }
+    }
 
-        return $serie;
+    private function criarEpisodios(Temporada $temporada, int $ep_por_temporada): void
+    {
+        for ($j = 1; $j <= $ep_por_temporada; $j++) {
+            $temporada->episodios()->create(['numero' => $j]);
+        }
     }
 }
