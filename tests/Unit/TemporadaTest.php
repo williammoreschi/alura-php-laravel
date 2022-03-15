3bcd14e5c369e4f7c662a578d679d20bcb370416
williammoreschi <williammoreschi@gmail.com>
2022-03-15 01:15:53 +0000

    Primeiro contato com testes automatizados.

---
+++ tests/Unit/TemporadaTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Episodio;
+use App\Temporada;
+use Tests\TestCase;
+
+class TemporadaTest extends TestCase
+{
+    /** @var Temporada */
+    private $temporada;
+    public function setUp(): void
+    {
+        parent::setUp();
+        $temporada = new Temporada();
+        $episodio1 = new Episodio();
+        $episodio1->assistido = true;
+        $episodio2 = new Episodio();
+        $episodio2->assistido = false;
+        $episodio3 = new Episodio();
+        $episodio3->assistido = true;
+
+        $temporada->episodios->add($episodio1);
+        $temporada->episodios->add($episodio2);
+        $temporada->episodios->add($episodio3);
+
+        $this->temporada = $temporada;
+    }
+
+    public function testBuscaPorEpisodiosAssistidos(): void
+    {
+
+
+        $episodiosAssistidos = $this->temporada->getEpisodiosAssistidos();
+
+        $this->assertCount(2, $episodiosAssistidos);
+
+        foreach ($episodiosAssistidos as $episodio) {
+            $this->assertTrue($episodio->assistido);
+        }
+    }
+
+    public function testBuscaTodosOsEpisodios(): void
+    {
+        $episodios = $this->temporada->episodios;
+        $this->assertCount(3, $episodios);
+    }
+}
