648a04cc2be012f6a8bc89c6076085276adeb22e
williammoreschi <williammoreschi@gmail.com>
2022-03-15 01:22:17 +0000

    Teste de inserir um série com uma temporada e um episódio

---
+++ tests/Unit/CriadorDeSerieTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Serie;
+use App\Services\CriadorDeSerie;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CriadorDeSerieTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testCriaSerie()
+    {
+        $criadorDeSerie = new CriadorDeSerie();
+        $nomeDaSerie = "Série de Teste";
+        $serieCriada = $criadorDeSerie->criarSerie($nomeDaSerie, 1, 1);
+
+        $this->assertInstanceOf(Serie::class, $serieCriada);
+        $this->assertDatabaseHas('series', ['nome' => $nomeDaSerie]);
+        $this->assertDatabaseHas(
+            'temporadas',
+            ['serie_id' => $serieCriada->id, 'numero' => 1]
+        );
+        $this->assertDatabaseHas('episodios', ['numero' => 1]);
+    }
+}
