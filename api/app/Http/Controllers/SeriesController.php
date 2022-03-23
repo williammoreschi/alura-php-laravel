3283d513427801119f21d2ce1e07f34f5ceb65b5
williammoreschi <williammoreschi@gmail.com>
2022-03-23 03:18:58 +0000

    Crud Series

--- api/app/Http/Controllers/SeriesController.php
+++ api/app/Http/Controllers/SeriesController.php
@@ -2,12 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use App\Series;
-use Illuminate\Http\Response;
+use App\Serie;
+use Illuminate\Http\Request;
 
-class SeriesController {
-    public function index(Response $response)
+class SeriesController
+{
+    public function index()
     {
-        return Series::all();
+        return Serie::all();
+    }
+
+    public function store(Request $request)
+    {
+        return response()
+            ->json(
+                Serie::create(['nome' => $request->nome]),
+                201
+            );
+    }
+
+    public function show(int $id)
+    {
+        $serie = Serie::find($id);
+        $status = (!is_null($serie)) ? 200 : 204;
+        return response()->json($serie, $status);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $serie = Serie::find($id);
+        if (is_null($serie)) {
+            return response()->json(['erro' => 'Recurso não existente'], 404);
+        }
+        $serie->fill($request->all());
+        $serie->save();
+        return response()->json($serie, 200);
+    }
+
+    public function destroy(int $id)
+    {
+        $qtdeRecursosRemovidos = Serie::destroy($id);
+        if ($qtdeRecursosRemovidos === 0) {
+            return response()->json(['erro' => 'Recurso não existente'], 404);
+        }
+        return response()->json('', 204);
     }
 }
