98100562fd4d0bf5a447293544c7a5c04f0b8fc5
williammoreschi <williammoreschi@gmail.com>
2022-03-26 03:45:44 +0000

    JWT Parte 1 - protejendo as rotas

--- api/app/Providers/AuthServiceProvider.php
+++ api/app/Providers/AuthServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use App\User;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Illuminate\Auth\GenericUser;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -30,10 +34,17 @@ class AuthServiceProvider extends ServiceProvider
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
 
-        $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+        $this->app['auth']->viaRequest('api', function (Request $request) {
+            if (!$request->hasHeader('Authorization')) {
+                return null;
             }
+            $authorization = $request->header('Authorization');
+            $token = str_replace("Bearer ","",$authorization);
+            
+            $data = JWT::decode($token, new Key('key_example','HS256'));
+
+            return new GenericUser(['email'=>$data['email']]);
+            //return User::where('email', $data['email'])->first();
         });
     }
 }
--- api/routes/web.php
+++ api/routes/web.php
@@ -17,7 +17,7 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api'], function () use ($router) {
+$router->group(['prefix' => 'api', 'middleware' => 'auth'], function () use ($router) {
     $router->group(['prefix' => 'series'],function() use($router){
         $router->post('', 'SeriesController@store');
         $router->get('', 'SeriesController@index');
